4ddc1f628465a2acc721a0d30a666144322d5e27
Filippeo <BulawaaFilip@gmail.com>
2022-07-02 15:05:09 +0200

    yiyu

--- script.js
+++ script.js
@@ -17,77 +17,44 @@ let offsetTime = 0;
 
 const timeStorage = window.localStorage;
 readSavedTime();
-// splitTable.textContent = timeStorage.splitTable;
-//  ds
+getTime();
+
 function getTime() {
   nowTime = Date.now();
 
   if (stop) {
     console.log("stopped");
     stoppedTime = nowTime - startTime - timeWhenStopped;
-    localStorage.setItem("stoppedTime", stoppedTime);
   } else {
     console.log("live");
-    localStorage.setItem("stoppedTime", stoppedTime);
   }
+
+  localStorage.setItem("stoppedTime", stoppedTime);
+
   time = nowTime - startTime - stoppedTime + offsetTime;
   mainTimer.textContent = timerDisplay(time);
 
-  // console.log(nowTime);
-  // console.log(startTime);
-  // console.log(stoppedTime);
-  // console.log(offsetTime);
-  // console.log(stop);
+  console.log(nowTime);
+  console.log(startTime);
+  console.log(stoppedTime);
+  console.log(timeWhenStopped);
+  console.log(offsetTime);
+  console.log(stop);
+  console.log(timeStorage.stopState);
 
   window.requestAnimationFrame(getTime);
   return time;
 }
-getTime();
 
-startButton.addEventListener("click", function () {
-  localStorage.setItem("startTime", startTime);
-  stop = false;
-  // localStorage.setItem('stop', stop);
-});
-
+startButton.addEventListener("click", start);
 resetButton.addEventListener("click", reset);
-
-pauseButton.addEventListener("click", function () {
-  stop = true;
-  pause();
-});
-
+pauseButton.addEventListener("click", pause);
 splitButton.addEventListener("click", split);
-
-function timerDisplay(timeInMiliseconds) {
-  let h, m, s, ms;
-  h = Math.floor(timeInMiliseconds / 1000 / 60 / 60);
-  m = Math.floor((timeInMiliseconds / 1000 / 60 / 60 - h) * 60);
-  s = Math.floor(((timeInMiliseconds / 1000 / 60 / 60 - h) * 60 - m) * 60);
-  ms = Math.floor(
-    (((timeInMiliseconds / 1000 / 60 / 60 - h) * 60 - m) * 60 - s) * 100
-  );
-
-  ms < 10 ? (ms = `0${ms}`) : (ms = `${ms}`);
-  s < 10 ? (s = `0${s}`) : (s = `${s}`);
-  m < 10 ? (m = `0${m}`) : (m = `${m}`);
-  h < 10 ? (h = `0${h}`) : (h = `${h}`);
-
-  // display adding minute and hour display only when there's any
-  if (m < 1) {
-    return `${s}:${ms}`;
-  } else if (h < 1) {
-    return `${m}:${s}:${ms}`;
-  } else {
-    return `${h}:${m}:${s}:${ms}`;
-  }
-}
 
 window.addEventListener("keydown", function (e) {
   switch (e.key) {
     case " ":
-      stop = !stop;
-      pause();
+      stop ? start() : pause(); // is it stopped? If yes, start the timer, if not, pause it
       return;
     case "Escape":
       reset();
@@ -95,9 +62,10 @@ window.addEventListener("keydown", function (e) {
   }
 });
 
-function pause() {
-  timeWhenStopped = time;
-  // localStorage.setItem('stop', stop);
+function start() {
+  localStorage.setItem("startTime", startTime);
+  stop = false;
+  // localStorage.setItem("stopState", stop);
 }
 
 function reset() {
@@ -121,11 +89,18 @@ function reset() {
   //   console.log(splitTimes[i].textContent);
   // }
 
-  // if (true /*cell at the left is empty*/) {
-  //   // delete the entire row
-  // } else if (false /* cell at the left is not empty */) {
-  //   // dont do that}
-  // }
+  if (true /*cell at the left is empty*/) {
+    // delete the entire row
+  } else if (false /* cell at the left is not empty */) {
+    // dont do that}
+  }
+}
+
+function pause() {
+  stop = true;
+  timeWhenStopped = time;
+  // localStorage.setItem("timeWhenStopped", timeWhenStopped);
+  // localStorage.setItem("stopState", stop);
 }
 
 function split() {
@@ -156,7 +131,7 @@ function readSavedTime() {
   if (timeStorage.startTime) {
     console.log(timeStorage.length);
 
-    timeWhenStopped = Number(timeStorage.timeWhenStopped);
+    // timeWhenStopped = Number(timeStorage.timeWhenStopped);
     startTime = Number(timeStorage.startTime);
     console.log(startTime);
     stoppedTime = Number(timeStorage.stoppedTime);
@@ -164,7 +139,9 @@ function readSavedTime() {
     // localStorage.setItem("splitTableState", splitTable.innerHTML);
     splitTable.innerHTML = timeStorage.splitTableState;
 
-    // stop = Boolean(timeStorage.stop);
+    // stop = Boolean(timeStorage.stopState);
+    console.log(stop);
+
     return;
   } else {
     timeWhenStopped = 0;
@@ -174,5 +151,29 @@ function readSavedTime() {
     localStorage.setItem("splitTableState", splitTable.innerHTML);
 
     return;
+  }
+}
+
+function timerDisplay(timeInMiliseconds) {
+  let h, m, s, ms;
+  h = Math.floor(timeInMiliseconds / 1000 / 60 / 60);
+  m = Math.floor((timeInMiliseconds / 1000 / 60 / 60 - h) * 60);
+  s = Math.floor(((timeInMiliseconds / 1000 / 60 / 60 - h) * 60 - m) * 60);
+  ms = Math.floor(
+    (((timeInMiliseconds / 1000 / 60 / 60 - h) * 60 - m) * 60 - s) * 100
+  );
+
+  ms < 10 ? (ms = `0${ms}`) : (ms = `${ms}`);
+  s < 10 ? (s = `0${s}`) : (s = `${s}`);
+  m < 10 ? (m = `0${m}`) : (m = `${m}`);
+  h < 10 ? (h = `0${h}`) : (h = `${h}`);
+
+  // display adding minute and hour display only when there's any
+  if (m < 1) {
+    return `${s}:${ms}`;
+  } else if (h < 1) {
+    return `${m}:${s}:${ms}`;
+  } else {
+    return `${h}:${m}:${s}:${ms}`;
   }
 }
